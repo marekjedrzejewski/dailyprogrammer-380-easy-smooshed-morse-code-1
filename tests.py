438aa8b07efa1ce44fc21bb7761cf2793d4756e7
Marek Jędrzejewski <kontakt@marekjedrzejewski.pl>
2020-08-15 16:27:45 +0200

    Add tests for basic challenge

---
+++ tests.py
@@ -0,0 +1,15 @@
+import unittest
+from smorse import smooshed_morse
+
+
+class TestSmooshedMorse(unittest.TestCase):
+    def test_smooshed_morse(self):
+        self.assertEqual(smooshed_morse('sos'), '...---...')
+        self.assertEqual(smooshed_morse('daily'), '...-...-..-.--')
+        self.assertEqual(smooshed_morse('programmer'), '.--..-.-----..-..-----..-.')
+        self.assertEqual(smooshed_morse('bits'), '-.....-...')
+        self.assertEqual(smooshed_morse('three'), '-.....-...')
+
+
+if __name__ == '__main__':
+    unittest.main()
